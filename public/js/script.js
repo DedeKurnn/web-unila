05cfdbf36b0675a3d851b60c8fc14288a1e37521
DedeKurnn <dedekurniawan.dike@gmail.com>
2023-04-19 20:07:33 +0700

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -10,8 +10,19 @@ window.onscroll = function() {
     }
 }
 
+// Navbar menu
 const navBtn = document.querySelector('#nav-btn');
-
+const navMenu = document.querySelector('#nav-menu');
 navBtn.addEventListener('click', function() {
     navBtn.classList.toggle('nav-active');
+    navMenu.classList.toggle('hidden');
 })
+
+// Dropdown
+const dropdownDefaultButton = document.querySelector('#dropdownDefaultButton');
+const dropdownMenu = document.querySelector('#dropdown');
+let lastOpened = null;
+
+dropdownDefaultButton.addEventListener('click', function() {
+    dropdownMenu.classList.toggle('hidden');
+});
