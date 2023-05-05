f0c5edfe472adbae28781853b89615750671502a
Dede Kurniawan <70650114+DedeKurnn@users.noreply.github.com>
2023-05-05 18:41:21 +0700

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -46,26 +46,3 @@ tabs.forEach (tab => {
     });
 });
 
-// For map
-// Where you want to render the map.
-var element = document.getElementById('osm-map');
-
-// Height has to be set. You can do this in CSS too.
-element.style = 'height:500px;';
-
-// Create Leaflet map on map element.
-var map = L.map(element);
-
-// Add OSM tile layer to the Leaflet map.
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// Target's GPS coordinates.
-var target = L.latLng('-5.3643552', '105.2413564');
-
-// Set map's center to target with zoom 14.
-map.setView(target, 16);
-
-// Place a marker on the same location.
-L.marker(target).addTo(map);
